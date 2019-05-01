ed2bd5f321397ccdbc2115ad555a701ca974d8bb
Shubham Rathi <shubhrathi02@gmail.com>
2019-05-01 22:39:22 +0530

    Initial Commit

---
+++ src/main/java/learn/customer/info/CustomerInfoApplication.java
@@ -0,0 +1,11 @@
+package learn.customer.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInfoApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CustomerInfoApplication.class, args);
+    }
+}
---
+++ src/main/java/learn/customer/info/controller/CustomerInfoController.java
@@ -0,0 +1,30 @@
+package learn.customer.info.controller;
+
+import learn.customer.info.models.CustomerInfo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerInfoController {
+
+    @RequestMapping("/")
+    public List<CustomerInfo> getAllCustomers(){
+        CustomerInfo c = new CustomerInfo();
+        c.setName("abc");
+        return Arrays.asList(c);
+    }
+
+    @RequestMapping("/{id}/")
+    public CustomerInfo getCustomerbyId(@PathVariable int id){
+        CustomerInfo c = new CustomerInfo();
+        c.setName(Integer.toString(id));
+        return c;
+    }
+
+
+}
