2b0625a94078fdeabcde0bc3f2d0a8dc168fb8e3
Shubham Rathi <shubhrathi02@gmail.com>
2019-05-02 02:05:19 +0530

    Testing

--- src/main/java/learn/customer/info/controller/CustomerInfoController.java
+++ src/main/java/learn/customer/info/controller/CustomerInfoController.java
@@ -1,29 +1,31 @@
 package learn.customer.info.controller;
 
 import learn.customer.info.models.CustomerInfo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
-@RequestMapping("/customer")
+@Controller
 public class CustomerInfoController {
 
     @RequestMapping("/")
+    @ResponseBody
     public List<CustomerInfo> getAllCustomers(){
         CustomerInfo c = new CustomerInfo();
         c.setName("abc");
         return Arrays.asList(c);
     }
 
-    @RequestMapping("/{id}/")
+    /*@RequestMapping("/{id}/")
     public CustomerInfo getCustomerbyId(@PathVariable int id){
         CustomerInfo c = new CustomerInfo();
         c.setName(Integer.toString(id));
         return c;
-    }
+    }*/
 
 }
