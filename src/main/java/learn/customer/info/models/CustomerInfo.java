2aab644767f707ef681752ae0f62c9e092000a89
Shubham Rathi <shubhrathi02@gmail.com>
2019-05-04 18:46:43 +0530

    Added database config H2 and Hikari

--- src/main/java/learn/customer/info/models/CustomerInfo.java
+++ src/main/java/learn/customer/info/models/CustomerInfo.java
@@ -1,6 +1,15 @@
 package learn.customer.info.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class CustomerInfo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     private String name;
 
